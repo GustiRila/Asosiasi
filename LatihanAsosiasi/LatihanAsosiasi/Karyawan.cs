cab25b3c35777bdb81f997dd0b9e54bfb15b472d
GustiRila <praja.rila31@gmail.com>
2019-07-02 14:53:57 +0700

    Asosiasi

---
+++ LatihanAsosiasi/LatihanAsosiasi/Karyawan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAsosiasi
+{
+    public class Karyawan
+    {
+        public string Nama { get; set; }
+        public bool Status { get; set; }
+        
+        public bool Login (Kartu kartu)
+        {
+            return kartu.IsValid(this);
+        }
+    }
+}
